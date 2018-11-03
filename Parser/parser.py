648139cd3a753d8e64d9089e4d82ea39fa10f904
ssjokhla <ssjokhla@gmail.com>
2018-11-03 15:44:46 -0400

    Created parser script to print out Summary from ics

--- Parser/parser.py
+++ Parser/parser.py
@@ -8,7 +8,7 @@ from pytz import UTC
 openFile = open('example.ics','rb')
 
 openCalendar = Calendar.from_ical(openFile.read())
-for comonent in openCalendar.walk():
-    print component.name
+for component in openCalendar.walk():
+    print(component.get('summary'))
 
 openFile.close()
