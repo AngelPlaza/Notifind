ab2fd147c9b8d2ac7bbf3d014dd15abb29631f2f
ssjokhla <ssjokhla@gmail.com>
2018-11-04 13:04:16 -0500

    Set up automate.sh to run with my MySQL Database

--- Parser/automate.sh
+++ Parser/automate.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-curl http://25livepub.collegenet.com/calendars/NJIT_EVENTS.ics > /home/chris/Notifind/Parser/NJIT_EVENTS.ics
-python3 /home/chris/Notifind/Parser/MySQLTest.py
+curl http://25livepub.collegenet.com/calendars/NJIT_EVENTS.ics > /home/samish/Notifind/Parser/NJIT_EVENTS.ics
+python3 /home/samish/Notifind/Parser/MySQLTest.py
 
-curl http://25livepub.collegenet.com/calendars/NJIT_EVENTS.ics?search=food > /home/chris/Notifind/Parser/NJIT_EVENTS.ics
-python3 /home/chris/Notifind/Parser/MySQLTest.py
+curl http://25livepub.collegenet.com/calendars/NJIT_EVENTS.ics?search=food > /home/samish/Notifind/Parser/NJIT_EVENTS.ics
+python3 /home/samish/Notifind/Parser/MySQLTest.py
 
-curl http://25livepub.collegenet.com/calendars/NJIT_EVENTS.ics?search=pizza > /home/chris/Notifind/Parser/NJIT_EVENTS.ics
-python3 /home/chris/Notifind/Parser/MySQLTest.py
+curl http://25livepub.collegenet.com/calendars/NJIT_EVENTS.ics?search=pizza > /home/samish/Notifind/Parser/NJIT_EVENTS.ics
+python3 /home/samish/Notifind/Parser/MySQLTest.py
 
-curl http://25livepub.collegenet.com/calendars/NJIT_EVENTS.ics?search=free > /home/chris/Notifind/Parser/NJIT_EVENTS.ics
-python3 /home/chris/Notifind/Parser/MySQLTest.py
+curl http://25livepub.collegenet.com/calendars/NJIT_EVENTS.ics?search=free > /home/samish/Notifind/Parser/NJIT_EVENTS.ics
+python3 /home/samish/Notifind/Parser/MySQLTest.py
+
+echo "Crontab ran"
 
